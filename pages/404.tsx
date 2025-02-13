3398e1b9220de07ab8f608d54b2b1493e5b95f0b
junaidkt01 <junaidktkmn@gmail.com>
2025-02-13 12:02:27 +0530

    added 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image"
+import "./globals.css"
+
+const Page404 = () => {
+  return (
+    <div className="page-404">
+      <div><Image src="/404.png" alt="" width={500} height={500} /></div>
+      {/* <div><img src="/404.png" alt="" /></div> */}
+    </div>
+  )
+}
+
+export default Page404
