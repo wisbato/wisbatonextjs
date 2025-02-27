af717473b156f29e84ab7aab420a76b3d93ff15a
junaidkt01 <junaidktkmn@gmail.com>
2025-02-27 15:29:50 +0530

    added google site verification on head tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      {/* <Head /> */}
+      <Head>
+        <meta name="google-site-verification" content="So7SGPFUbY5sEqy_n2caQLf28Murin6Had4cO1F94nk" />
+      </Head>
       <body>
         <Main />
         <NextScript />
